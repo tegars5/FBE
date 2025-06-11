946f286028a4a8066127fbe43ef6f4bda747a6f4
Tegar Saputra <tegar0651@gmail.com>
2025-06-11 14:02:41 +0700

    perbaikan

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -1,21 +1,30 @@
 @props([
     'styles' => true,
 ])
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
     @vite(['resources/js/main.js'])
-    @if($styles)
-    @vite(['resources/css/style.css'])
+    @if ($styles)
+        @vite(['resources/css/style.css'])
     @else
-    @vite(['resources/css/dashboard.css'])
+        @vite(['resources/css/dashboard.css'])
     @endif
-   
+
     {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    {{-- link icons --}}
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+
 
     <title>{{ $title ?? 'Pt FujiyamaBiomasEnergy' }}</title>
 </head>
