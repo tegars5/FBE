200206b89e0b12788aaaf77a090535cf5a988003
Tegar Saputra <tegar0651@gmail.com>
2025-06-12 14:57:13 +0700

    perbaikan head

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- link icons --}}
     <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
